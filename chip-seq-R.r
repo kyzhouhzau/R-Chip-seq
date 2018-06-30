a03ce6d6258701c8900a109703a8801fa05788ba
Kaiyinzhou <33114591+kyzhouhzau@users.noreply.github.com>
2018-06-30 19:01:11 +0800

    Update and rename chip-seq-R to chip-seq-R.r

--- chip-seq-R.r
+++ chip-seq-R.r
@@ -10,6 +10,11 @@ txdb <- TxDb.Athaliana.BioMart.plantsmart28
 peak <- readPeakFile("C:\\Users\\zhouk\\Desktop\\GSM2476321_ARR1.2ypet_S89S99_LD_p6BA_3D.peak.bed")
 #显示peak在基因上的位置
 covplot(peak)
+
+#promoter <- getPromoters(TxDb=txdb,upstream=300, downstream=300)
+#tagMatrix <- getTagMatrix(peak, windows=promoter)
+#tagMatrixList <- lapply(peak, getTagMatrix,windows=promoter)
+#plotAvgProf(tagMatrix, xlim=c(-300, 300),conf=0.95,resample=500, facet="row")
 #注释
 peakAnno <- annotatePeak(peak, tssRegion=c(-3000, 3000),TxDb=txdb, annoDb="org.At.tair.db")
 #注释结果可视化分析
@@ -20,3 +25,4 @@ upsetplot(peakAnno)#复合图
 #peak附近基因可视化
 peakAnnoList <- lapply(peak, annotatePeak, TxDb=txdb,tssRegion=c(-3000, 3000), verbose=FALSE)
 plotAnnoBar(peakAnnoList)#bar图
+
